f56da58070eb3472845913ccfbcadf313b0e094a
Reda El Marzouki <96263971+Raderne@users.noreply.github.com>
2022-11-17 13:20:03 +0300

    burger menu edited

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 /* ===== nav ===== */
-const navMenu = document.getElementById("nav-menu");
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+/*========MENU SHOW========*/
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
